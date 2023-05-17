072eb4fdc72f031bfa06c9eaba45bcfd009b1c55
Harshad Gupta <harshad90@gmail.com>
2023-05-17 11:06:54 +0100

    Update recover_test.go

--- middleware/recover_test.go
+++ middleware/recover_test.go
@@ -8,7 +8,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/AndrewBewseyTNA/echo/v4"
+	"github.com/merc90/echo/v4"
 	"github.com/labstack/gommon/log"
 	"github.com/stretchr/testify/assert"
 )
